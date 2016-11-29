ef4fc2c4ab0a8efc93011a498cacdac8dbff72d3
julian <julianroche@gmail.com>
2016-11-29 17:22:03 +0000

    Extracted sim functions into separate lib files. Changed version number.

---
+++ lib/powerVersusNoise.R
@@ -0,0 +1,122 @@
+#' A function to simulate power while varying the amount of noise.
+#' 
+#' @param functions A list of functional forms to be evaluated
+#' @param distribution The source distribtion, uniform by default
+#' @param nsim The number of datasets used to estimate the rejection reject regions for an alternative with level alpha
+#' @param num.noise Number of different noise levels used
+#' @param nl The total level of noise
+#' @param n Number of data points per simulation
+powerVersusNoise <- function(functions, 
+                             distribution=runif, 
+                             nsim = 500, 
+                             alpha=0.05, 
+                             nl=3, 
+                             num.noise=30, 
+                             n=320, ...) {
+  # Vectors holding the null and alternative "associations" for each of the nsim null datasets at a given noise level
+  val.cor=val.dcor=val.mine=val.A=val.spear=rep(NA,nsim)        
+  val.cor2=val.dcor2=val.mine2=val.A2=val.spear2= rep(NA,nsim)        
+  
+  # Arrays holding the estimated power for each of the association types, 
+  #for each functional with each noise level
+  power.cor=power.dcor=power.mine=power.A=power.spear= array(NA, c(length(functions),num.noise))                
+  
+  #Loop through the noise level and functional form; 
+  #each time we estimate a null distribution based on the marginals of the data, 
+  #and then use that null distribution to estimate power
+  for(l in 1:num.noise)
+  {
+    for(typ in 1:length(functions))
+    {
+      ## This next loop simulates data under the null with the correct marginals (x is uniform, and y is a function of a uniform with gaussian noise)
+      
+      cat(sprintf("Noise Null %s; Type %s\n",  l, typ))
+      for(ii in 1:nsim)
+      {
+        #sample from the given distribution
+        x <- distribution(n,...)
+        
+        #evaluate the function 
+        y <- functions[[typ]](x, nl, l, num.noise, n)
+        
+        # We resimulate x so that we have the null scenario
+        x <- distribution(n,...)
+        
+        # calculate the association
+        val.cor[ii]=(cor(x,y))^2            
+        val.dcor[ii]=dcor(x,y)            
+        val.mine[ii]=mine(x,y)$MIC            
+        val.A[ii]=ma(data.frame(x,y))$A             
+        val.spear[ii]=(cor(x,y,method='spearman'))^2            
+      }
+      
+      # remove the mic trials which glitch
+      val.mine <- val.mine[which(!is.na(val.mine))]                 
+      
+      # Next we calculate our rejection cutoffs
+      cut.cor <- quantile(val.cor,1-alpha)
+      cut.dcor <- quantile(val.dcor,1-alpha)
+      cut.mine <- quantile(val.mine,1-alpha)
+      cut.A <- quantile(val.A,1-alpha)
+      cut.spear <- quantile(val.spear,1-alpha)
+      
+      ## Next we simulate the data again, this time under the alternative
+      cat(sprintf("Noise Alt %s; Type %s;\n",  l, typ))
+      for(ii in 1:nsim)
+      {
+        #the dependent data
+        x = distribution(n,...)
+        y=functions[[typ]](x, nl, l, num.noise, n)
+        
+        # calculate the associations
+        val.cor2[ii]=(cor(x,y))^2
+        val.dcor2[ii]=dcor(x,y)
+        val.mine2[ii]=mine(x,y)$MIC
+        val.A2[ii]=ma(data.frame(x,y))$A
+        val.spear2[ii]=(cor(x,y,method='spearman'))^2
+      }
+      
+      
+      ## Now we estimate the power as the number of alternative statistics exceeding our estimated cutoffs
+      power.cor[typ,l] <- sum(val.cor2 > cut.cor)/nsim
+      power.dcor[typ,l] <- sum(val.dcor2 > cut.dcor)/nsim
+      power.mine[typ,l] <- notNA.greater(val.mine2, cut.mine)
+      power.A[typ,l] <- sum(val.A2 > cut.A)/nsim
+      power.spear[typ,l] <- sum(val.spear2 > cut.spear)/nsim
+    }
+  }
+  
+  #assemble the results
+  powerdf <- data.frame(power.cor)
+  powerdf <- t(powerdf)
+  colnames(powerdf) <- names(functions)
+  
+  results <- melt(powerdf)
+  results <- cbind(results,rep('Pearson',num.noise))
+  results <- cbind(results,(1:num.noise)/10)
+  colnames(results) <- c('var','Form','Power','Statistic','Noise')
+  results <- combineNoiseResults(results,power.dcor,'dcor',num.noise)
+  results <- combineNoiseResults(results,power.mine,'MIC',num.noise)
+  results <- combineNoiseResults(results,power.A,'A',num.noise)
+  results <- combineNoiseResults(results,power.spear,'Spearman',num.noise)
+  
+  #reorder based on the functional form
+  results$Form <- reorder(results$Form, new.order=names(functions))
+  results <- results[ order(results$Form), ]
+  
+  results
+}
+
+#a utility function to build up the noise results
+combineNoiseResults <- function(df, x, stat, num.noise)
+{
+  tf <- data.frame(x)
+  tf <- t(tf)
+  colnames(tf) <- names(functions)
+  tf <- melt(tf)
+  tf <- cbind(tf,rep(stat,num.noise))
+  tf <- cbind(tf,(1:num.noise)/10)
+  colnames(tf) <- c('var','Form','Power','Statistic','Noise')
+  df <- rbind(df,tf)
+  df
+}
