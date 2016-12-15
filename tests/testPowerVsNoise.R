c92987102abe631cd8b009a78c256615db27752a
julian <julianroche@gmail.com>
2016-12-15 15:52:41 +0000

    Migrated function types and associated benchmarks to nlf package.

--- tests/testPowerVsNoise.R
+++ tests/testPowerVsNoise.R
@@ -2,13 +2,12 @@ library(ProjectTemplate)
 load.project()
 library(nlf)
 
-
 #install.packages('minerva')
 #library(minerva)
 #test parameter
 
-rm(linearCpp)
-types <- c("quadraticCpp", "cubicCpp", "qrootCpp")
+#get all function types from the nlf package
+types <- ls(getNamespace("nlf"), all.names=F)
 noise <- 3
 numNoise <- 30
 
@@ -22,20 +21,22 @@ ggplot(res, aes(noiseLevel, power, colour=measure)) +
   facet_grid(n~Function)+
   theme(legend.position="bottom")
 
-#3d surface 
-library(plotly)
-powerEstimate <- res[res$measure == 'Pearson' & res$Function == "cubicCpp",c(1,2,5)]
-powerEstimate <- spread(powerEstimate, noiseLevel, power)
-rownames(powerEstimate) <- powerEstimate[,1]
-powerEstimate<- as.matrix(powerEstimate[,-1])
-p <- plot_ly(z = powerEstimate, 
-             x = as.numeric(colnames(powerEstimate)), 
-             y = as.numeric(rownames(powerEstimate)), 
-             type = "surface")%>% 
-  layout(title = "Power: Pearson Correlation",
-         scene = list(
-           xaxis = list(title = "Noise"), 
-           yaxis = list(title = "n"), 
-           zaxis = list(title = "Power")))
-p
+#3d surface of a single association measure for a single function type.
+plotSurface <- function(res, measure, type) {
+  powerEstimate <- res[res$measure == measure & res$Function == type, c(1,2,5)]
+  powerEstimate <- spread(powerEstimate, noiseLevel, power)
+  rownames(powerEstimate) <- powerEstimate[,1]
+  powerEstimate<- as.matrix(powerEstimate[,-1])
+  p <- plot_ly(z = powerEstimate, 
+               x = as.numeric(colnames(powerEstimate)), 
+               y = as.numeric(rownames(powerEstimate)), 
+               type = "surface")%>% 
+    layout(title = paste("Power of", measure, "for", type),
+           scene = list(
+             xaxis = list(title = "Noise"), 
+             yaxis = list(title = "n"), 
+             zaxis = list(title = "Power")))
+  p
+}
 
+plotSurface(res, "Pearson", "sigmoidCpp")
